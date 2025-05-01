10a440c028e305f24092dd236dc5d9d0e6b251e0
Adrian Smith <smith.adriane@gmail.com>
2025-05-01 13:48:00 -0700

    turn off debug tap for now.

--- src/com/phronemophobic/clobber.clj
+++ src/com/phronemophobic/clobber.clj
@@ -1935,7 +1935,7 @@
                       [[:update $editor #(editor-backward-char %)]]
 
                       :else
-                      (do (tap> s)
+                      (do #_(tap> s)
                           nil)))
                   )
                 body)
