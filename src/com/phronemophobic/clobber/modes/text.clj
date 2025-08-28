8849885a5dfb633c8b82803f0ff3f7448b3ac6a5
Adrian Smith <smith.adriane@gmail.com>
2025-08-28 12:43:51 -0700

    remove dead code.

--- src/com/phronemophobic/clobber/modes/text.clj
+++ src/com/phronemophobic/clobber/modes/text.clj
@@ -1426,62 +1426,6 @@
        ;; else, no match
        editor))))
 
-(comment
-  (-> (make-editor)
-      )
-  
-  ,)
-
-(defn editor-repeat-isearch-forward [editor]
-  (let [search-state (::search editor)
-        _ (assert search-state)
-
-        query (:query search-state)
-        
-        search-cursor (:cursor editor)
-        search-index (:char search-cursor)
-        regexp (Pattern/compile query
-                                (bit-or
-                                 Pattern/CASE_INSENSITIVE
-                                 Pattern/LITERAL))
-        ^Rope
-        rope (:rope editor)
-        matcher (.matcher regexp rope)
-
-        match (if (.find matcher search-index)
-                (.toMatchResult matcher)
-                (when (.find matcher 0)
-                  (.toMatchResult matcher)))
-        ;; now do it again
-        match (when match
-                (if (.find matcher)
-                  (.toMatchResult matcher)
-                  (when (.find matcher 0)
-                    (.toMatchResult matcher))))
-        
-        search-state (-> search-state
-                         (assoc :query query)
-                         (assoc :match match))
-        
-        editor (assoc editor ::search search-state)]
-    (if match
-      (let [ ;; calculate new cursor
-            s (-> (.subSequence rope 0 (.start match))
-                  .toString)
-            
-            {:keys [row column-byte]} (util/count-row-column-bytes s)
-            
-            cursor {:byte (alength (.getBytes s "utf-8"))
-                    :char (.length s)
-                    :point (util/num-points s)
-                    :row row
-                    :column-byte column-byte}]
-        (assoc editor :cursor cursor))
-      ;; else, no match
-      editor)))
-
-
-
 (defn ^:private reverse-char-sequence 
   ^CharSequence
   [^CharSequence cs]
