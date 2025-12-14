84c5c46cf05d1f5b8f65e8ca62da757787feeb01
Adrian Smith <smith.adriane@gmail.com>
2025-12-13 17:34:41 -0800

    Simplify defaults for opening editors.

--- src/com/phronemophobic/clobber/editor.clj
+++ src/com/phronemophobic/clobber/editor.clj
@@ -16,19 +16,13 @@
     (when (not= -1 idx)
       (subs fname idx))))
 
+
+
 (defn guess-mode
-  "Creates a new editor.
-  
-  Returns a map with `:editor` and `:ui`.
-  
-  If `:mode` is provided, use that mode. Otherwise, try to guess the right mode.
-  
-  If `:source` is provided, us that. Otherwise, load from `:file`."
-  [{:keys [file source mode] :as opts}]
+  [{:keys [file source] :as opts}]
   (let [mode
         (cond
-          (or (= mode :clojure)
-              (when file
+          (or (when file
                 (#{".edn" ".clj" ".cljc"} (file-ext file)))
               (:ns opts))
           :clojure
@@ -38,20 +32,29 @@
           :org
 
           :else :text)]
-    (case mode
-      :clojure
-      {:editor (cui/make-editor opts)
-       :ui #'com.phronemophobic.clobber.modes.clojure.ui/code-editor}
-      
-      :org
-      {:editor (oui/make-editor opts)
-       :ui #'com.phronemophobic.clobber.modes.org.ui/org-editor}
+    mode))
 
-      ;; else
-      {:editor (tui/make-editor opts)
-       :ui #'com.phronemophobic.clobber.modes.text.ui/text-editor})))
+(defn editor-ui [mode]
+  (case mode
+    :clojure #'com.phronemophobic.clobber.modes.clojure.ui/code-editor
+    :org #'com.phronemophobic.clobber.modes.org.ui/org-editor
+    ;; else
+    #'com.phronemophobic.clobber.modes.text.ui/text-editor))
 
-
-
-
-
+(defn make-editor
+  "Creates a new editor with associated ui.
+  
+  Returns a map with `:editor` and `:ui`.
+  
+  If `:mode` is provided, use that mode. Otherwise, try to guess the right mode.
+  
+  If `:source` is provided, use that. Otherwise, load from `:file`."
+  ([opts]
+   (case (or (:mode opts)
+             (guess-mode opts))
+     :clojure (cui/make-editor opts)
+     
+     :org (oui/make-editor opts)
+     
+     ;; else
+     (tui/make-editor opts))))
